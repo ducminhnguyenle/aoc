d2748e5ffca977bf51883b10c5d7154643587615
ducminhnguyenle <ducminhnguyenle30091996@gmail.com>
2023-12-09 21:42:01 +0700

    Solution for day02 part1

---
+++ aoc2023/day02/day02_p1.py
@@ -0,0 +1,23 @@
+# Part 1
+bag = {"red": 12,
+       "green": 13,
+       "blue": 14}
+
+with open("/mnt/hdd/dminh/aoc/aoc2023/day02/day02_input.txt", "r") as f:
+    input = [line.strip() for line in f.readlines()]
+    
+    result_p1 = 0
+    for line in input:
+        possible = True
+        id, events = line.split(":")
+        
+        for event in events.split(";"):
+            
+            for collection in event.split(","):
+                n, color = collection.split()
+                if int(n) > bag.get(color, 0):
+                    possible = False
+        if possible:
+            result_p1 += int(id.split()[-1])
+            
+    print(result_p1)
